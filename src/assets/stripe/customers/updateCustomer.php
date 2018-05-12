787624c7db57e572a09d0f8cfb2cbe1bfc8d3406
Trey Hope <treyhope@Treys-MacBook-Air.local>
2018-05-12 16:53:02 -0400

    Working with json better for http calls.

--- src/assets/stripe/customers/updateCustomer.php
+++ src/assets/stripe/customers/updateCustomer.php
@@ -1,58 +1,52 @@
 <?php 
-    require_once($_SERVER['DOCUMENT_ROOT']."/assets/vendor/autoload.php");
+    require_once($_SERVER['DOCUMENT_ROOT'].'/assets/vendor/autoload.php');
 
-    //Required parameters.
-    $apiKey = $_POST['apiKey'];
-    $customerId = $_POST['customerId'];
-    
-    if(isset($apiKey)){
-        try{
-            //Set API Key.
-            \Stripe\Stripe::setApiKey($apiKey);
-            
-            //Retrieve customer.
-            $customer = \Stripe\Customer::retrieve($customerId);
+    $data = (array) json_decode(file_get_contents('php://input'), TRUE);
 
-            //Update customer description
-            if(!empty($_POST['description'])){
-              $customer->description = $_POST['description'];
-            }
+    try{
+      //Set API Key.
+      \Stripe\Stripe::setApiKey($data['apiKey']);
+      
+      //Retrieve customer.
+      $customer = \Stripe\Customer::retrieve($data['customerId']);
 
-            //Update customer source.
-            if(!empty($_POST['token'])){
-              $customer->source = $_POST['token'];
-            }
+      //Update customer shipping.
+      if(!is_null($data['email'])){
+        $customer->email = $data['email'];
+      }
 
-            //Update customer shipping information.
-            if(!empty($_POST['shipping'])){
-              $customer->shipping = $_POST['shipping'];
-            }
+      //Update customer description
+      if(!is_null($data['description'])){
+          $customer->description = $data['description'];
+      }
 
-            //Save customer.
-            $customer->save();
+      //Update customer shipping.
+      if(!is_null($data['shipping'])){
+          $customer->shipping = $data['shipping'];
+      }
 
-            echo $customer->__toJSON();
-          }catch(\Stripe\Error\Card $e) {
-            echo $e->__toJSON();
-          } catch (\Stripe\Error\RateLimit $e) {
-            // Too many requests made to the API too quickly
-            echo $e->__toJSON();
-          } catch (\Stripe\Error\InvalidRequest $e) {
-            // Invalid parameters were supplied to Stripe's API
-            echo $e->__toJSON();
-          } catch (\Stripe\Error\Authentication $e) {
-            // Authentication with Stripe's API failed
-            echo $e->__toJSON();
-          } catch (\Stripe\Error\ApiConnection $e) {
-            // Network communication with Stripe failed
-            echo $e->__toJSON();
-          } catch (\Stripe\Error\Base $e) {
-            echo $e->__toJSON();
-          } catch (Exception $e) {
-            // Something else happened, completely unrelated to Stripe
-            echo $e->__toJSON();
-          }         
-    }else{
-        echo "Api key not set, cannot access. Goodbye.";
-    }
+      //Save customer.
+      $customer->save();
+
+      echo $customer->__toJSON();
+    }catch(\Stripe\Error\Card $e) {
+      echo $e->__toJSON();
+    } catch (\Stripe\Error\RateLimit $e) {
+      // Too many requests made to the API too quickly
+      echo $e->__toJSON();
+    } catch (\Stripe\Error\InvalidRequest $e) {
+      // Invalid parameters were supplied to Stripe's API
+      echo $e->__toJSON();
+    } catch (\Stripe\Error\Authentication $e) {
+      // Authentication with Stripe's API failed
+      echo $e->__toJSON();
+    } catch (\Stripe\Error\ApiConnection $e) {
+      // Network communication with Stripe failed
+      echo $e->__toJSON();
+    } catch (\Stripe\Error\Base $e) {
+      echo $e->__toJSON();
+    } catch (Exception $e) {
+      // Something else happened, completely unrelated to Stripe
+      echo $e->__toJSON();
+    }  
 ?>
