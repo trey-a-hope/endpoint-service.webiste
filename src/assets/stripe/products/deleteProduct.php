0d4b4e114a53e2510370752c8bba5870afa8d1d9
Trey Hope <treyhope@Treys-MacBook-Air.local>
2018-05-26 17:42:46 -0400

    Created deleteProduct endpoint.

---
+++ src/assets/stripe/products/deleteProduct.php
@@ -0,0 +1,37 @@
+<?php 
+    require_once($_SERVER['DOCUMENT_ROOT'].'/assets/vendor/autoload.php');
+
+    $data = (array) json_decode(file_get_contents('php://input'), TRUE);
+
+    try{
+        //Set API Key.
+        \Stripe\Stripe::setApiKey($data['apiKey']);
+
+        //Retrieve product.
+        $product = \Stripe\Product::retrieve($data['productId']);
+
+        //Delete product.
+        $product->delete();
+
+        echo $product->__toJSON();
+    }catch(\Stripe\Error\Card $e) {
+      echo $e->getMessage();
+    } catch (\Stripe\Error\RateLimit $e) {
+      // Too many requests made to the API too quickly
+      echo $e->getMessage();
+    } catch (\Stripe\Error\InvalidRequest $e) {
+      // Invalid parameters were supplied to Stripe's API
+      echo $e->getMessage();
+    } catch (\Stripe\Error\Authentication $e) {
+      // Authentication with Stripe's API failed
+      echo $e->getMessage();
+    } catch (\Stripe\Error\ApiConnection $e) {
+      // Network communication with Stripe failed
+      echo $e->getMessage();
+    } catch (\Stripe\Error\Base $e) {
+      echo $e->getMessage();
+    } catch (Exception $e) {
+      // Something else happened, completely unrelated to Stripe
+      echo $e->getMessage();
+    }  
+?>
