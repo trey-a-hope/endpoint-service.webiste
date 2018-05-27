1e756c5afdc9b1e6e760d969525dc2db74d79d89
Trey Hope <treyhope@Treys-MacBook-Air.local>
2018-05-27 03:18:07 -0400

    Created endpoints for coupons.

---
+++ src/assets/stripe/coupons/createCoupon.php
@@ -0,0 +1,42 @@
+<?php 
+    require_once($_SERVER['DOCUMENT_ROOT'].'/assets/vendor/autoload.php');
+
+    $data = (array) json_decode(file_get_contents('php://input'), TRUE);
+
+    try{
+        //Set API Key.
+        \Stripe\Stripe::setApiKey($data['apiKey']);
+
+        //Create coupon.
+        $coupon = \Stripe\Coupon::create(
+            array(
+                'percent_off' => $data['percent_off'],
+                'amount_off' => $data['amount_off'],
+                'duration' => $data['duration'],
+                'duration_in_months' => $data['duration_in_months'],
+                'id' => $data['id']
+            )
+        );
+
+        echo $coupon->__toJSON();
+    }catch(\Stripe\Error\Card $e) {
+      echo $e->__toJSON();
+    } catch (\Stripe\Error\RateLimit $e) {
+      // Too many requests made to the API too quickly
+      echo $e->__toJSON();
+    } catch (\Stripe\Error\InvalidRequest $e) {
+      // Invalid parameters were supplied to Stripe's API
+      echo $e->__toJSON();
+    } catch (\Stripe\Error\Authentication $e) {
+      // Authentication with Stripe's API failed
+      echo $e->__toJSON();
+    } catch (\Stripe\Error\ApiConnection $e) {
+      // Network communication with Stripe failed
+      echo $e->__toJSON();
+    } catch (\Stripe\Error\Base $e) {
+      echo $e->__toJSON();
+    } catch (Exception $e) {
+      // Something else happened, completely unrelated to Stripe
+      echo $e->__toJSON();
+    }  
+?>
---
+++ src/assets/stripe/coupons/deleteCoupon.php
@@ -0,0 +1,37 @@
+<?php 
+    require_once($_SERVER['DOCUMENT_ROOT'].'/assets/vendor/autoload.php');
+
+    $data = (array) json_decode(file_get_contents('php://input'), TRUE);
+
+    try{
+        //Set API Key.
+        \Stripe\Stripe::setApiKey($data['apiKey']);
+
+        //Retrieve coupon.
+        $coupon = \Stripe\Coupon::retrieve($data['id']);
+
+        //Delete coupon.
+        $coupon->delete();
+
+        echo $coupon ->__toJSON();
+    }catch(\Stripe\Error\Card $e) {
+      echo $e->__toJSON();
+    } catch (\Stripe\Error\RateLimit $e) {
+      // Too many requests made to the API too quickly
+      echo $e->__toJSON();
+    } catch (\Stripe\Error\InvalidRequest $e) {
+      // Invalid parameters were supplied to Stripe's API
+      echo $e->__toJSON();
+    } catch (\Stripe\Error\Authentication $e) {
+      // Authentication with Stripe's API failed
+      echo $e->__toJSON();
+    } catch (\Stripe\Error\ApiConnection $e) {
+      // Network communication with Stripe failed
+      echo $e->__toJSON();
+    } catch (\Stripe\Error\Base $e) {
+      echo $e->__toJSON();
+    } catch (Exception $e) {
+      // Something else happened, completely unrelated to Stripe
+      echo $e->__toJSON();
+    }  
+?>
